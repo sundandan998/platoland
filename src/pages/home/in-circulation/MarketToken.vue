59665ec0e7772380766f50233992f3001b7eb408
sundandan <sundandan@pld.com>
2020-02-28 11:30:42 +0800

    修改从交易市场进入通证大全展示问题

--- src/pages/home/in-circulation/MarketToken.vue
+++ src/pages/home/in-circulation/MarketToken.vue
@@ -36,6 +36,8 @@
         assetsData: '',
         radio: true,
         index: "0",
+        selectRadio: '',
+        is_collection: '',
       }
     },
     created() {
@@ -44,22 +46,22 @@
     methods: {
       // 列表信息
       list() {
-        // this.$route.params
-        api.tokenList({ category: 'all' }).then(res => {
+        // debugger
+        api.tokenList().then(res => {
           this.assetsData = res.data
-          if (this.$route.params.code == 'LIFE+') {
-            this.assetsData[2].is_collection = false
-          }else if(this.$route.params.code == 'PLD'){
-            this.assetsData[0].is_collection = false
+          // 循环得到code和is_collectio,
+          for (let i = 0; i < this.assetsData.length; i++) {
+            this.selectRadio = this.assetsData[i].code
+            this.is_collection = this.assetsData[i].is_collection
+            // 判断如果从deal页面传来的code==本页面的code就改变本页的is_collection==true,
+            // 其他is_collection为false
+            if (this.$route.params.code == this.selectRadio) {
+              this.assetsData[i].is_collection=true
+            } else {
+              this.assetsData[i].is_collection=false
+            }
           }
-          // this.assetsData[2].is_collection = false
-          // console.log(this.assetsData.subject)
-          // for(let i=0;i<this.assetsData.length;i++){
-          //   this.selectRadio=this.assetsData[i].code
-          //   console.log(this.selectRadio)
-          // }
         }).catch(err => {
-          console.log(err)
         })
       },
     }
--- src/pages/mine/assets/Assets.vue
+++ src/pages/mine/assets/Assets.vue
@@ -16,7 +16,7 @@
         <img src="../../../assets/images/u3511.png" alt><span>{{$t('m.addAssets')}}</span>
       </router-link>
     </div>
-    <div class="certificate-list-card" v-for="(item,index) in assetsdata">
+    <div class="certificate-list-card" v-for="(item,index) in assetsdata" v-if="item.token.subject!=null">
       <router-link :to="{name:'AssetsDetailed',params:{id:item.id,code:item.token.code}}">
         <!-- <mt-cell :title="item.token.code + '('+item.token.nickname+')'" :value="item.balance|keepTwoNum"
           :label="item.token.subject" class="certificate-list-card-cell">
